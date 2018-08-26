e57d6e2ae5ecf0a4dae56c28b124de84706e8090
Jonathan Branam <jonathan.branam@gmail.com>
2018-08-25 22:35:01 -0400

    Customize pelican theme.

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,7 +12,16 @@ TIMEZONE = 'America/Indiana/Indianapolis'
 
 DEFAULT_LANG = 'en'
 
+STATIC_PATHS = ['images']
+
 THEME = '/Volumes/user/jbranam/work/pelican-themes/taman'
+
+# taman theme
+USER_LOGO_URL = 'images/JPB_headshot.jpg'
+TAGLINE = '''
+Data Scientist and Engineer
+'''
+# end taman theme
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -24,14 +33,14 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         )
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('twitter', 'JonathanBranam'),
+          ('linkedin', 'JonathanBranam'),
+          ('github', 'jonathanbranam'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
